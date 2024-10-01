63afcffe07c6544e0e1fbd31df0fd01c6dd8e568
Michael Corry <corryvert@gmail.com>
2024-10-01 23:12:39 +0300

    UI tweaks

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
         <section className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I'm Corry</h1>
-                <p className={styles.description}>{`I'm a fullstack developer with 1 year of experience using React and NodeJS. Reach out if you'd like to learn more!`}</p>
+                <p className={styles.description}>{`A fullstack developer with 1 year of experience using React and NextJS. Reach out if you'd like to learn more!`}</p>
                 <a href='mailto:michaelcorry01@gmail.com' className={styles.contactBtn}>Contact Me</a>
             </div>
             <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg} />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
 
     return (
         <nav className={styles.navbar}>
-            <a className={styles.title} href="/">Portfolio</a>
+            <a className={styles.title} href="/">My Portfolio</a>
             <div className={styles.menu}>
                 <img className={styles.menuBtn} src={menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")} alt="menu-button" onClick={() => setMenuOpen(!menuOpen)} />
                 <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
@@ -21,7 +21,7 @@ export const Navbar = () => {
                         <a href="#projects">Projects</a>
                     </li>
                     <li>
-                        <a href="#contact">Contanct</a>
+                        <a href="#contact">Contact</a>
                     </li>
                 </ul>
             </div>
