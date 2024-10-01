94704011280438db30bb8c5055be588d0a699454
Michael Corry <corryvert@gmail.com>
2024-10-01 17:32:04 +0300

    Portfolio website using React

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Navbar } from './components/Navbar/Navbar';
 import { Hero } from './components/Hero/Hero';
 import { About } from './components/About/About';
 import { Experience } from './components/Experience/Experience';
-import { Projects } from './components/Projects/projects';
+import { Projects } from './components/Projects/Projects';
 import { Contact } from './components/Contact/Contact';
 
 function App() {
