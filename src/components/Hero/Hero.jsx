2e909489aef50c3eb8143e5c29930d97b4bd5b35
Michael Corry <corryvert@gmail.com>
2024-10-03 09:51:41 +0300

    Changed avatar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ export const Hero = () => {
                 <p className={styles.description}>{`A fullstack developer with 1 year of experience using React and NextJS. Reach out if you'd like to learn more!`}</p>
                 <a href='mailto:michaelcorry01@gmail.com' className={styles.contactBtn}>Contact Me</a>
             </div>
-            <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg} />
+            <img src={getImageUrl("hero/heroNew.png")} alt="Hero image of me" className={styles.heroImg} />
             <div className={styles.topBlur} />
             <div className={styles.bottomBlur} />
         </section>
