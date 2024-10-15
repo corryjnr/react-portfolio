1bc6a9de58246e063df4f01e2e536fcaf12d840f
Michael Corry <corryvert@gmail.com>
2024-10-15 23:53:20 +0300

    Added mouse tracking glow and SVG icons

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,49 @@ import { About } from './components/About/About';
 import { Experience } from './components/Experience/Experience';
 import { Projects } from './components/Projects/Projects';
 import { Contact } from './components/Contact/Contact';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setPosition({ x: event.pageX, y: event.pageY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  const glowStyle = {
+    position: 'absolute',
+    top: position.y, // Offset to center the glow
+    left: position.x,
+    width: '0.1px',
+    height: '0.1px', // Offset to center the glow
+    borderRadius: '50%',
+    backgroundColor: 'rgba(59, 130, 246, .5)', // Glow color
+    boxShadow: '0 0 1000px 300px rgba(0, 150, 255, 0.07)', // Glow effect
+    pointerEvents: 'none', // Ensures the glow doesn’t block interaction with other elements
+    zIndex: 999, // Ensure it stays on top of everything
+  };
 
   return (
     <div className={styles.App}>
-      <div className={styles.firstPage}>
-        <Navbar />
-        <Hero />
+      <div className="App">
+        <div style={glowStyle}></div>
+        <div className={styles.firstPage}>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Projects />
+        <Contact />
       </div>
-      <About />
-      <Experience />
-      <Projects />
-      <Contact />
     </div>
   )
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import { getImageUrl } from '../../utils'
 import styles from "./Contact.module.css"
+import GithubLogo from './GithubLogo';
+import LinkedinLogo from './LinkedinLogo'
+import GmailLogo from './GmailLogo'
 
 export const Contact = () => {
+    let blue = '#57cbff'
+    let primary = '#8892b0'
+    let [gitIconColor, setGitIconColor] = React.useState(primary);
+    let [gmailIconColor, setGmailIconColor] = React.useState(primary);
+    let [linkedinIconColor, setLinkedinIconColor] = React.useState(primary);
     return (
         <footer id='contact' className={styles.container}>
             <div className={styles.text}>
@@ -11,19 +19,26 @@ export const Contact = () => {
             </div>
             <ul className={styles.links}>
                 <li className={styles.link}>
-                    <img src={getImageUrl("contact/emailIcon.png")} alt='Email Icon' />
-                    <a href="mailto:michaelcorry01@gmail.com">michaelcorry01@gmail.com</a>
+                    <a href="mailto:michaelcorry01@gmail.com" onMouseOver={() => setGmailIconColor(blue)}
+                        onMouseOut={() => setGmailIconColor(primary)}>
+                        <GmailLogo stroke={gmailIconColor}
+                        />
+                    </a>
                 </li>
                 <li className={styles.link}>
-                    <img src={getImageUrl("contact/linkedinIcon.png")} alt='Linkedin Icon' />
-                    <a href="https://www.linkedin.com/in/michaelcorry1">linkedin.com/in/michaelcorry1</a>
+                    <a href="https://www.linkedin.com/in/michaelcorry1" onMouseOver={() => setLinkedinIconColor(blue)}
+                        onMouseOut={() => setLinkedinIconColor(primary)}>
+                        <LinkedinLogo stroke={linkedinIconColor} />
+                    </a>
                 </li>
                 <li className={styles.link}>
-                    <img src={getImageUrl("contact/githubIcon.png")} alt='Github Icon' />
-                    <a href="https://www.github.com/corryjnr">github.com/corryjnr</a>
+                    <a href="https://www.github.com/corryjnr" onMouseOver={() => setGitIconColor(blue)}
+                        onMouseOut={() => setGitIconColor(primary)}>
+                        <GithubLogo stroke={gitIconColor} />
+                    </a>
                 </li>
             </ul>
-
+            <p>Designed & Built by Michael Corry</p>
         </footer>
     )
 }
